021d26edbb84d0331aa33394d46314abb56b8baf
tanmaydewan786-source <tanmay.dewan786@gmail.com>
2025-12-02 16:55:14 +0530

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    // Parallax effect prototype
-    document.querySelector('.hero').onmousemove = function(e) {
-        this.style.backgroundPositionX = 50 + (e.clientX/40) + '%';
-    };
-    // Mobile nav toggle, etc.
-};
